6d367dc9a9fa947f8bc2025a2903822d4bd56a00
Max Vigueras <mvt.2000@hotmail.com>
2024-12-17 17:48:44 -0600

    UI: List View Bands added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:band_names/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Band Names App',
+      initialRoute: 'home',
+      routes: {
+        'home': ( _ ) => const HomeScreen()
+      },
     );
   }
 }
